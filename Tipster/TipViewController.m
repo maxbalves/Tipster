d5826808ed3891e4eb23ffa32992a6767979dd50
Max Bagatini Alves <maxbagatini@hotmail.com>
2022-06-15 10:15:26 -0700

    Added files to repo

---
+++ Tipster/TipViewController.m
@@ -0,0 +1,46 @@
+//
+//  TipViewController.m
+//  Tipster
+//
+//  Created by Max Bagatini Alves on 6/15/22.
+//
+
+#import "TipViewController.h"
+
+@interface TipViewController ()
+@property (weak, nonatomic) IBOutlet UITextField *billField;
+@property (weak, nonatomic) IBOutlet UILabel *tipLabel;
+@property (weak, nonatomic) IBOutlet UILabel *totalLabel;
+@property (weak, nonatomic) IBOutlet UISegmentedControl *tipSegCtrl;
+
+@end
+
+@implementation TipViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (IBAction)onTap:(id)sender {
+    NSLog(@"Tapped!");
+    [self.view endEditing:true];
+}
+
+- (IBAction)updateTotal:(id)sender {
+    // Set the labels to sample strings
+    self.tipLabel.text = @"$20.00";
+    self.totalLabel.text = @"$120.00";
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
